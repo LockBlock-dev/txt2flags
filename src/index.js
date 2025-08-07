1a6f4f38b040df8eaf80aea7be5f580e2577bf25
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2025-08-07 18:13:10 +0200

    Add offset option, flags count and rework the text processing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,85 @@
-import flags from "./flags.js";
+import flagsData from "./flags.js";
+
+const options = new Map([["offset", 0]]);
+const stats = new Map([["count", 0]]);
+const flags = new Map(flagsData);
 
 const textTextArea = document.getElementById("text");
-const flagsTextArea = document.getElementById("flags");
+const resultTextArea = document.getElementById("result");
 
-const handleOnTextInput = () => {
-    let text = textTextArea.value.toLowerCase();
-    const chars = text.match(/[\S]{1,2}/g) || [];
+const processText = () => {
+    const text = textTextArea.value.toLowerCase();
+    const offset = options.get("offset");
 
-    chars.forEach((char) => {
-        const flag = flags.find((flag) => flag[0] === char);
+    stats.set("count", 0);
 
-        console.log(char, flag);
+    if (text.length < 2) {
+        resultTextArea.value = text;
+        updateStats();
+        return;
+    }
 
-        if (!flag) return;
+    let result = "";
+    let count = 0;
 
-        text = text.replace(char, flag[1]);
-    });
+    if (offset !== 0) result += text.substring(0, offset);
 
-    flagsTextArea.value = text;
+    for (let i = offset; i < text.length; i += 2) {
+        const twoChar = text.substring(i, i + 2);
+
+        if (flags.has(twoChar)) {
+            result += flags.get(twoChar);
+            count++;
+        } else {
+            result += twoChar;
+        }
+    }
+
+    stats.set("count", count);
+
+    resultTextArea.value = result;
+
+    updateStats();
 };
 
-textTextArea.addEventListener("input", () => handleOnTextInput());
+const initializeOptions = () => {
+    options.forEach((value, key) => {
+        const optionInput = document.getElementById(key);
+        const optionValueDisplay = document.getElementById(`${key}_value`);
+
+        if (!optionInput || !optionValueDisplay) {
+            console.warn(`Option '${key}' element not found!`);
+            return;
+        }
+
+        optionInput.value = value;
+        optionValueDisplay.innerText = value;
+
+        optionInput.addEventListener("input", ({ target: { value } }) => {
+            const newValue = parseInt(value, 10);
+
+            options.set(key, newValue);
+
+            optionValueDisplay.innerText = newValue;
+
+            processText();
+        });
+    });
+};
+
+const updateStats = () => {
+    stats.forEach((value, key) => {
+        const statValueDisplay = document.getElementById(`${key}_value`);
+
+        if (statValueDisplay) statValueDisplay.innerText = value;
+    });
+};
+
+const main = () => {
+    initializeOptions();
+    updateStats();
+
+    textTextArea.addEventListener("input", processText);
+};
+
+main();
